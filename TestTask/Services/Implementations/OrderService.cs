9f777cda8507bf4fa25547f7fcbb621dd3e1a052
lesha <lesha@DESKTOP-F31VJOV>
2024-05-14 12:37:17 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            return await _context.Orders
+                .OrderByDescending(x => x.CreatedAt).Where( x => x.Quantity > 1).FirstAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _context.Orders
+                .Where(x => x.User.Status == UserStatus.Active)
+                .OrderBy(x => x.CreatedAt).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly int firstTaskYear = 2003;
+        private readonly int secondTaskYear = 2010;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            //делаю действия с orders тут, потому что нельзя изменять OrderService, а других путей найти не смог
+            var selectedOrdersList = await _context.Orders.
+                Where(x => x.CreatedAt.Year == firstTaskYear && x.Status == Enums.OrderStatus.Delivered).ToListAsync();
+            var userId = selectedOrdersList.GroupBy(x => x.UserId)
+                                  .Select(s => new { Id = s.Key, TotalSum = s.Select(s2 => s2.Price).Sum() })
+                                  .OrderByDescending(x => x.TotalSum).First().Id;
+            return await _context.Users.Where(x => x.Id == userId).FirstAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users.Where(x => x.Orders
+                .Any(x => x.CreatedAt.Year == secondTaskYear && x.Status == Enums.OrderStatus.Paid)).ToListAsync();
+        }
+    }
+}
